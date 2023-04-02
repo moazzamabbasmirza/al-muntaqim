c80da4beb39ad08bd15a6c359ecc33e96449130f
Moazzam Abbas <moazzamabbas89@gmail.com>
2023-04-03 04:20:27 +0500

    sss

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+      <footer
+        container={false}
+        class="bg-white rounded-lg shadow dark:bg-gray-900 m-4"
+      >
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div class="sm:flex sm:items-center sm:justify-between">
             <a
