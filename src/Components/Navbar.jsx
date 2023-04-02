cd7e21c5c38134eba464683eb24897d52aa7b890
Moazzam Abbas <moazzamabbas89@gmail.com>
2023-04-03 04:51:33 +0500

    sss

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
               alt="Flowbite Logo"
             />
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Al-Muntaqim
             </span>
           </a>
           <button
@@ -62,6 +62,14 @@ const Navbar = () => {
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Pricing
                 </a>
               </li>
               <li>
