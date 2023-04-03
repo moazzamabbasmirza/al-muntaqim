8fb6c4710d714ecf0f430a7cdb1f18d098301344
Moazzam Abbas <moazzamabbas89@gmail.com>
2023-04-03 17:50:10 +0500

    sss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Services from './Components/Services';
 
 function App() {
   return (
     <div classNameName="App">
-      <Home />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>    </div>
   );
 }
 
