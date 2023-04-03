36d13ae29576d7f1632249fd185a172c0b9ac184
Moazzam Abbas <moazzamabbas89@gmail.com>
2023-04-03 23:29:29 +0500

    sss

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Link to="/">
-            <a href="https://flowbite.com/" className="flex items-center">
+            <span href="https://flowbite.com/" className="flex items-center">
               <img
                 src="https://flowbite.com/docs/images/logo.svg"
                 className="h-8 mr-3"
@@ -15,7 +15,7 @@ const Navbar = () => {
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Al-Muntaqim
               </span>
-            </a>
+            </span>
           </Link>
           <button
             data-collapse-toggle="navbar-default"
@@ -43,43 +43,43 @@ const Navbar = () => {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link to="/home">
-                  <a
+                  <span
                     href="/"
                     className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                     aria-current="page"
                   >
                     Home
-                  </a>
+                  </span>
                 </Link>
               </li>
               <li>
                 <Link to="/about">
-                  <a
+                  <span
                     href="/about"
                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     About
-                  </a>
+                  </span>
                 </Link>
               </li>
               <li>
                 <Link to="/services">
-                  <a
+                  <span
                     href="/"
                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     Services
-                  </a>
+                  </span>
                 </Link>
               </li>
               <li>
                 <Link to="/contact">
-                  <a
+                  <span
                     href="/"
                     className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >
                     Contact
-                  </a>
+                  </span>
                 </Link>
               </li>
             </ul>
